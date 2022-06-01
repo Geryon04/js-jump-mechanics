f0b51c8df73b032bc9bb485c933795f97b77e3cd
Geryon04 <juanjavierzerofour@gmail.com>
2022-06-01 19:30:04 -0300

    ball function added

--- jump.js
+++ jump.js
@@ -3,3 +3,10 @@ var pencil = canvas.getContext("2d");
 
 pencil.fillStyle = "gray",
 pencil.fillRect(0, 0, 700, 500);
+
+function ball(x, y){
+    pencil.fillStyle = blue;
+    pencil.beginPath();
+    pencil.arc(x, y, 10, 0, Math.PI * 2);
+    pencil.fill();
+}
